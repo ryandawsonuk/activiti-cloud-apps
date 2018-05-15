52bd5d64b3cc8222b23adb0d38b9df8f719b3135
Ryan Dawson <ryan.dawson@alfresco.com>
2018-05-15 09:10:17 +0100

    add keycloak authentication

--- src/main/java/org/activiti/cloud/apps/AppsApplication.java
+++ src/main/java/org/activiti/cloud/apps/AppsApplication.java
@@ -2,8 +2,10 @@ package org.activiti.cloud.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.activiti.cloud.services.security","org.activiti.cloud.services.identity","org.activiti.cloud.alfresco"})
 public class AppsApplication {
 
     public static void main(String[] args) {
